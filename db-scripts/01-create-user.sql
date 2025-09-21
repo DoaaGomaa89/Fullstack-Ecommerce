1f3786250def389439459e9e463464f9037fd3cd
Doaa Gomaa <engdoa,adbelhady@yahoo.com>
2025-09-21 23:02:29 +0930

    quick fix

--- db-scripts/01-create-user.sql
+++ db-scripts/01-create-user.sql
@@ -1,15 +1,4 @@
-CREATE USER 'ecommerceapp'@'localhost' IDENTIFIED BY 'ecommerceapp';
-
-GRANT ALL PRIVILEGES ON * . * TO 'ecommerceapp'@'localhost';
-
-#
-# Starting with MySQL 8.0.4, the MySQL team changed the 
-# default authentication plugin for MySQL server 
-# from mysql_native_password to caching_sha2_password.
-#
-# The command below will make the appropriate updates for your user account.
-#
-# See the MySQL Reference Manual for details: 
-# https://dev.mysql.com/doc/refman/8.0/en/caching-sha2-pluggable-authentication.html
-#
-ALTER USER 'ecommerceapp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ecommerceapp';
+-- Align with docker-compose env and avoid plugin directives
+CREATE USER IF NOT EXISTS 'admin' IDENTIFIED BY 'admin';
+GRANT ALL PRIVILEGES ON `full-stack-ecommerce`.* TO 'admin';
+FLUSH PRIVILEGES;
